5be501ae47b630b250c1b949f81a1dbdf9ef6a73
Gabriel Piccolella <gp125286@student.wpsok.org>
2024-08-21 11:14:27 -0700

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,18 @@
+#Name: Gabriel Piccolella
+#Class: 5th hour
+#Assignment: HW2
+
+#Print Hello World!
+print("Hello World!")
+
+#Print a question asking for your name and a way for you to respond
+print("What's your name?")
+input("")
+#Have it ask for two integers from the user
+x = input("Choose a number between 1 and 10")
+y = input("Choose a second number between 1 and 10")
+
+#Add the two integers together and print them out
+sum = int(x) + int(y)
+
+print("The sum of these numbers is", sum)
