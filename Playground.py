79a9a8a1b89ff06d63ffcbadd24b7be4961c63ea
Gabriel Piccolella <gp125286@student.wpsok.org>
2024-09-04 13:20:02 -0500

    Playground Project ft Merlin the Wise

---
+++ Playground.py
@@ -0,0 +1,10 @@
+#Defining the Name of the Adventurer
+print("Hello Adventurer!")
+print("My name is Merlin the Wise, tell me how shall i refer to thee?")
+name = input()
+print("Hello!," + name,",What a fantastic name!")
+
+#Transition to the next scene
+print("")
+
+
