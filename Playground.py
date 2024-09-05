87911832d7e66146047c4017691c6eb6fb57748a
Gabriel Piccolella <gp125286@student.wpsok.org>
2024-09-05 13:18:37 -0500

    Playground Project ft Merlin the Wise, Added if statements and nearly finished

--- Playground.py
+++ Playground.py
@@ -5,6 +5,27 @@ name = input()
 print("Hello!," + name,",What a fantastic name!")
 
 #Transition to the next scene
-print("")
+print("So " + name,",Tell me a little bit about yourself!")
+backstory = input()
+print("My oh my! You are quite the interesting figure!")
+
+#A Riddle By Merlin the Wise
+riddleList = ["Yes", "No"]
+print("Now listen here dearest adventurer, for I have a riddle for you")
+print("This riddle is difficult for seldom few know, answer this riddle with a simple Yes or No!")
+print("Now listen here all far and wide, for I am the greatest wizard of all! Do you Deny?")
+
+x = input()
+
+if x == "No":
+    print("Perfect answer!, Good luck on your future adventures friend!")
+
+elif x == "Yes":
+    print("What! How dare you! ")
+
+else:
+    print("You have failed the riddle! For this You shall be forever turned into a fiddle!")
+
+
 
 
