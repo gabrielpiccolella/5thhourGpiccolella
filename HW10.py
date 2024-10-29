e72f80f345664bba8349b4f4e34abcc2930488ec
Gabriel Piccolella <gp125286@student.wpsok.org>
2024-10-29 13:18:14 -0500

    Added Homework 10

---
+++ HW10.py
@@ -0,0 +1,32 @@
+#Name: Gabriel Piccolella
+#Class: 5th Hour
+#Assignment: HW10
+
+#1. Create a while loop with variable i that counts down from 5 to 0
+#and then prints "Hello World!" at the end.
+i = 5
+while i >= 0:
+    print(i)
+    i -=1
+else:
+    print("Hello World!")
+
+#2. Create a while loop that prints only even numbers
+#between 1 and 30.
+i = 0
+while i % 2 == 0:
+    print(i)
+    i += 2
+    if i >= 31:
+        break
+
+
+#3. Create a while loop that repeats until the user
+#inputs the number 0.
+n = input("enter a number")
+
+while n != 0:
+    n = input("enter a number")
+    print("User input is zero")
+    break
+
