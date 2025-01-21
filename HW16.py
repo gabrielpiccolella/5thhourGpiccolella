8c75056490e05fe03cefafa25aeafccf64312524
Gabriel Piccolella <gp125286@student.wpsok.org>
2025-01-21 13:18:56 -0600

    Completed Homework 16

--- HW16.py
+++ HW16.py
@@ -1,56 +1,50 @@
 #Name: Gabriel Piccolella
 #Class: 5th Hour
 #Assignment: HW16
+
 import random
+
 #1. Create a def function that plays a single round of rock, paper, scissors where the user inputs
 #1 for rock, 2 for paper, or 3 for scissors and compares it to a random number generated to serve
 #as the "opponent's hand".
 
-def rps():
-    playerHand = input("Choose a number between 1 and 3! 1 is rock, 2 is paper, and 3 is scissors.")
-    if playerHand == 1:
-       choice_name = 'Rock'
-    elif playerHand == 2:
-        choice_name = 'Paper'
+def rock_paper_scissors():
+   playerhand = int(input("pick a number 1-3 rock is 1, 2 is paper, 3 is scissors."))
+   opponenthand = random.randint(1, 3)
+   if playerhand == 1 and opponenthand == 3:
+       print("player picked rock, opponent picked scissors, player wins")
+   elif playerhand == 1 and opponenthand == 2:
+       print("player picked rock, opponent picked paper, opponent wins")
+   elif playerhand == 1 and opponenthand == 1:
+       print("player picked rock, opponent picked rock, a draw")
+
+   elif playerhand == 2 and opponenthand == 3:
+       print("player picked paper, opponent picked scissors, opponent wins")
+   elif playerhand == 2 and opponenthand == 2:
+       print("player picked paper, opponent picked paper, a draw")
+   elif playerhand == 2 and opponenthand == 1:
+       print("player picked paper, opponent picked rock, player wins")
+
+   elif playerhand == 3 and opponenthand == 3:
+       print("player picked scissors, opponent picked scissors, a draw")
+   elif playerhand == 3 and opponenthand == 2:
+       print("player picked scissors, opponent picked paper, player wins")
+   elif playerhand == 3 and opponenthand == 1:
+       print("player picked scissors, opponent picked rock, opponent wins")
+
+   repeat_game()
+
+
+
+def repeat_game():
+    replayInput = input("Would you like to play again? Y/N ")
+    if replayInput == "Y":
+        rock_paper_scissors()
     else:
-        choice_name = 'Scissors'
-
-    opponentsHand = random.randint(1,3)
-    if opponentsHand == 1:
-        comp_choice_name = 'Rock'
-    elif opponentsHand == 2:
-        comp_choice_name = 'Paper'
-    else:
-        comp_choice_name = 'Scissors'
-
-    print(choice_name, 'vs', comp_choice_name)
-
-    if playerHand == opponentsHand:
-        result = "DRAW"
-    elif (playerHand == 1 and opponentsHand == 2) or (opponentsHand == 1 and playerHand == 2):
-        result = 'Paper'
-    elif (playerHand == 1 and opponentsHand == 3) or (opponentsHand == 1 and playerHand == 3):
-        result = 'Rock'
-    elif (playerHand == 2 and opponentsHand == 3) or (opponentsHand == 2 and playerHand == 3):
-        result = 'Scissors'
-
-    if result == "DRAW":
-        print("It's a tie!")
-    elif result == choice_name:
-        print("User wins!")
-    else:
-        print("Computer wins!")
-
-
-
-
-#2. Create a def function that prompts the user to input if they want to play another round, and
-#repeats the RPS def function if they do or exits the code if they don't.
-def playAgain():
-    print("Do you want to play again? (Y/N)")
-    ans = input().lower()
-    if ans == 'n':
         exit()
 
 
-rps()
+
+
+rock_paper_scissors()
+
