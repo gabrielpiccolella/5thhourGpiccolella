1c9dc309d5dfd693d74b57895d83648c98f02b3a
Gabriel Piccolella <gp125286@student.wpsok.org>
2025-01-31 12:46:05 -0600

    did Homework 19

---
+++ HW19.py
@@ -0,0 +1,14 @@
+#Name: Gabriel Piccolella
+#Class: 5th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+from HW15 import hello_world
+from HW15 import average
+from HW15 import animales
+from HW15 import count
+from HW15 import numbers
+
+#2. Call the functions here and run HW19.
+
+#3. Delete all calls from HW15 and run HW19 again.
