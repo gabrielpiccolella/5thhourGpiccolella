3dbd1b79a681f5bfc5598abf755590c12a603655
Gabriel Piccolella <gp125286@student.wpsok.org>
2025-02-03 12:50:54 -0600

    DId SCenario 6

---
+++ Scenario 6.py
@@ -0,0 +1,24 @@
+#Name: Gabriel Piccolella
+#Class: 5th Hour
+#Assigment: Scenario 6
+import random
+
+#After an extended leave, the team lead for the RPG developer is back, and he wants to continue the project.
+#He wants to prototype the character creation model but first needs something that rolls stats for the characters.
+#He wants you to make a function that rolls 4 six-sided dice (d6), sorts them from highest to lowest, and then adds the
+#highest 3 together. He then wants you to add that result to a list outside the function. He wants you to run that function
+#5 more times (six times total) and print all six stats.
+def stat_roll():
+    for i in range(6):
+        stat1 = [random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)]
+
+        print(stat1)
+        stat1.sort(reverse=True)
+        print(stat1[0]+stat1[1]+stat1[2])
+
+stat_roll()
+
+
+
+#Once that is done, to ensure that the average of the statblock is fair (somewhere roughly between 12-13), he wants you
+#to plug it into a calculator (Scenario 7) and print the average.
---
+++ Scenario 7.py
@@ -0,0 +1,23 @@
+#Name:
+#Class: 5th Hour
+#Assignment: Scenario 7
+
+#Import all of Scenario 6 here
+from
+
+
+
+
+
+
+
+listAverage = 0
+
+def final_average():
+    global listAverage
+    listAverage = #Calculate the sum of the list by the length of the list here
+    return listAverage
+
+final_average()
+
+print(listAverage)
