c7c74e786be07683d5a535e6b2c9165f509d6091
Gabriel Piccolella <gp125286@student.wpsok.org>
2025-04-21 12:53:58 -0500

    Added Team 2's info and building block stages

---
+++ Team 2 Final Project building block stage.py
@@ -0,0 +1,17 @@
+#Names: Gabriel Piccolella, Gage Huntington, Ryley Ashcraft, Eric Lin.
+#Team: 2
+#Class: 5th Hour
+#Assignment: Final Project
+
+#Scrum Template: https://docs.google.com/document/d/1lj8xZ0Y5f0yIAYmHzDW40DiCe2NXuIek-VHHGVOzF1o/edit?usp=sharing
+#Scrum Document: [Put Yours Here]
+
+
+#Craps
+
+#necessity
+#dice system
+#1st
+#dice checker system (What number the dice lands on and what it correlates to)
+#Payout and bet system
+#winning/losing money
